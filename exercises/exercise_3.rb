2a619d2c5ddf7bb84ac7fc8e6e7d3cd288553623
Mike Ackison <mike.ackison@shopify.com>
2021-03-01 22:34:07 +0000

    delete/destroy store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -1,8 +1,17 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
 
 puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+# Load the third store (into @store3) as you did the other two before.
+# Using Active Record's destroy method, delete the store from the database.
+# Verify that the store has been deleted by again outputting (putsing) the count (as you did in Exercise 1.)
+
+@store3 = Store.find_by(id: 3)
+
+@store3.destroy
+
+puts "STORE COUNT---> #{Store.count}"
